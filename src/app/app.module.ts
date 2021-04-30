3e8224cd409fddd1ab8f35bf4e5a8e65a75a4a13
Jhon Soplopuco <67161649+Jhon-20@users.noreply.github.com>
2021-04-30 10:23:00 -0600

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
+import {​​ HttpClientModule }​​ from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { TodoModule } from './todo/todo.module';
+import { TodoService } from './todo/service/todo.service';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule,
+    TodoModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
